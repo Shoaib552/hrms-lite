f3404394e1b64264f8a3883ac6a4e964774ff3dd
shoaib552 <smohdshoaib208@gmail.com>
2026-01-27 14:37:03 +0530

    reupdate name

--- frontend/hrms-frontend/src/components/Navbar.js
+++ frontend/hrms-frontend/src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
   return (
     <div style={{ background: "#0f172a", padding: "16px" }}>
       <div className="container" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
-        <h2 style={{ color: "#ffffff" }}>HRMS Lite</h2>
+        <h2 style={{ color: "#ffffff" }}>PeopleOps HR</h2>
         <div style={{ display: "flex", gap: "16px" }}>
           <Link to="/" style={{ color: "#cbd5f5", textDecoration: "none" }}>Employees</Link>
           <Link to="/attendance" style={{ color: "#cbd5f5", textDecoration: "none" }}>Attendance</Link>
